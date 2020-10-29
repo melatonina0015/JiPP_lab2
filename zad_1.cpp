f91ff7289ee68cd2bcec1f1fb7074213fbdc50df
Amelia Kwiecinska <kwam0015@gmail.com>
2020-10-29 17:22:31 +0100

    complete zad_1

--- zad_1.cpp
+++ zad_1.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
+void mySwap(int *a, int *b)
+{
+    if(*b < *a){
+        int tmp = *a;
+        *a = *b;
+        *b = tmp;
+    }
+}
+
 int main()
 {
-    cout << "Hello Wolrd!\n";
+    int a, b;
+    cin >> a >> b;
+    mySwap(&a, &b);
+    cout << a << " " << b << endl;
 
     system("pause");
     return 0;
