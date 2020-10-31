f059818c4ee2e0dafcb468468ca35bc69deeb5d4
Amelia Kwiecinska <kwam0015@gmail.com>
2020-10-31 16:05:12 +0100

    Complete zad_3

---
+++ zad_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void mySwap(int &a, int *b)
+{
+    if(*b < a){
+        int tmp = a;
+        a = *b;
+        *b = tmp;
+    }
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    mySwap(a, &b);
+    cout << a << " " << b << endl;
+
+    system("pause");
+    return 0;
+}
